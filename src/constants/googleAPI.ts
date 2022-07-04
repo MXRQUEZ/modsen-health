071392a0e23c98e997fee99815122db15b795c4c
Rojkov Maxim <maximro.box@gmail.com>
2022-07-04 17:13:44 +0300

    moved folder constants

---
+++ src/constants/googleAPI.ts
@@ -0,0 +1,4 @@
+export const CLIENT_ID = "876132436532-t0o1jks4db75731auit91tej8jcr2h7h.apps.googleusercontent.com";
+export const API_KEY = "AIzaSyANazE5-1MHck5NwDpTH7Y-wEMMI2y5E14";
+export const DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
+export const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
