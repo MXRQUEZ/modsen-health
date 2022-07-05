db81ca2d94ab7a690ce95c7651a1fd6ffab90516
Rojkov Maxim <maximro.box@gmail.com>
2022-07-05 11:21:12 +0300

    Added doctors mock data

---
+++ src/constants/doctors.ts
@@ -0,0 +1,36 @@
+import { IDoctor } from "@/types/IDoctor";
+
+export const DOCTORS: IDoctor[] = [
+  {
+    name: "Juliana",
+    surname: "Peterson",
+    age: 29,
+    image: "https://discord.com/channels/@me/631923587078422558/993789223507349504",
+    quote: "Eat well, drink in moderation, and sleep sound, in these three good health abound.",
+    experience: 15,
+  },
+  {
+    name: "Johnny",
+    surname: "Sins",
+    age: 44,
+    image: "https://cdn.discordapp.com/attachments/631923587078422558/993789966419238972/unknown.png",
+    quote: "Early to bed and early to rise, makes a man healthy, wealthy and wise.",
+    experience: 25,
+  },
+  {
+    name: "Bred",
+    surname: "Tompson",
+    age: 36,
+    image: "https://cdn.discordapp.com/attachments/631923587078422558/993789328176205865/unknown.png",
+    quote: "Eat well, drink in moderation, and sleep sound, in these three good health abound.",
+    experience: 20,
+  },
+  {
+    name: "Shirley",
+    surname: "Jackson",
+    age: 40,
+    image: "https://cdn.discordapp.com/attachments/631923587078422558/993793030278623262/unknown.png",
+    quote: "Happiness is nothing more than good health and a bad memory.",
+    experience: 4,
+  },
+];
---
+++ src/types/IDoctor.ts
@@ -0,0 +1,8 @@
+export interface IDoctor {
+  name: string;
+  surname: string;
+  image: string;
+  age: number;
+  quote: string;
+  experience: number;
+}
