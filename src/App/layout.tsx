5c9ad6e2819065016e14bf04a5f9d50a85d2613d
Sergey Svistun <svistunss.py@gmail.com>
2022-07-07 17:32:43 +0300

    mui theme provider fixed

--- src/App/layout.tsx
+++ src/App/layout.tsx
@@ -1,26 +1,33 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import { createTheme, StyledEngineProvider, ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import { ThemeProvider as ScThemeProvider } from "styled-components";
 import Header from "@/components/Header";
 import GlobalStyles from "@/styles/styledGlobal";
 import { AppWrapper, MainStyled, AppContentContainer } from "@/styles/styledApp";
 import BackgroundArcs from "@/components/BackgroundArcs";
 import theme from "@/constants/theme";
 
+const muiTheme = createTheme();
+
 const Layout: FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <AppWrapper>
-        <BackgroundArcs />
-        <Header />
-        <MainStyled>
-          <AppContentContainer>
-            <Outlet />
-          </AppContentContainer>
-        </MainStyled>
-      </AppWrapper>
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <MuiThemeProvider theme={muiTheme}>
+        <ScThemeProvider theme={theme}>
+          <GlobalStyles />
+          <AppWrapper>
+            <BackgroundArcs />
+            <Header />
+            <MainStyled>
+              <AppContentContainer>
+                <Outlet />
+              </AppContentContainer>
+            </MainStyled>
+          </AppWrapper>
+        </ScThemeProvider>
+      </MuiThemeProvider>
+    </StyledEngineProvider>
   );
 };
 
